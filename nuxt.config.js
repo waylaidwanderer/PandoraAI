18bf2789ef6cfdc152fb785212fc35d70eaf20d2
Mike Evans <etnperlong@gmail.com>
2023-03-21 21:38:39 +0000

    feat: support PWA (Progressive Web Apps) (#21)
    * feat: new icon design
    
    * feat: initial support for PWA
    
    * fix: PWA icon maskable padding
    
    ---------
    
    Co-authored-by: anonymous <anonymous@protonmail.com>
    Co-authored-by: Joel <jcz@hey.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
     },
     modules: [
         '@nuxtjs/tailwindcss',
+        '@kevinmarrec/nuxt-pwa',
         'nuxt-icon',
         '@pinia/nuxt',
         '@vueuse/nuxt',
@@ -23,5 +24,25 @@ export default defineNuxtConfig({
     ],
     pinia: {
         autoImports: ['defineStore', 'acceptHMRUpdate'],
+    },
+    pwa: {
+        icon: {
+            source: './public/icon.png',
+            maskablePadding: 0,
+        },
+        meta: {
+            name: 'PandoraAI',
+            description: 'Multiple AI Web Chat Client',
+            theme_color: '#7733ff',
+            mobileAppIOS: true,
+            nativeUI: true,
+        },
+        manifest: {
+            name: 'PandoraAI',
+            description: 'Multiple AI Web Chat Client',
+            background_color: '#7733ff',
+            lang: 'en',
+            useWebmanifestExtension: false,
+        },
     },
 });
